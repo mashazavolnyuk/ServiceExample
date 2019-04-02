c7396a4d4cc3c5a8887d29dc349910b4a715a5f3
Masha Kishynskaya <mashazavolnyuk@yandex.ru>
2019-04-02 12:40:07 +0300

    Added example how start service from Android O.

--- app/src/main/java/com/mashazavolnyuk/serviceexample/MainActivity.java
+++ app/src/main/java/com/mashazavolnyuk/serviceexample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mashazavolnyuk.serviceexample;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,6 +29,12 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(new Intent(MainActivity.this,MyService.class));
+                }
+                else {
+                    startService(new Intent(MainActivity.this,MyService.class));
+                }
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
--- app/src/main/java/com/mashazavolnyuk/serviceexample/MyService.java
+++ app/src/main/java/com/mashazavolnyuk/serviceexample/MyService.java
@@ -1,4 +1,56 @@
 package com.mashazavolnyuk.serviceexample;
 
-public class MyService {
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    private static final String TAG = "MyService";
+    Notification notification;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            String channelId = "alarm_service";
+            NotificationChannel channel = new  NotificationChannel(channelId, "Alarm service", NotificationManager.IMPORTANCE_LOW);
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+            notification = new Notification.Builder(getApplicationContext(), channelId)
+                    .setContentTitle(BuildConfig.VERSION_NAME)
+                    .setContentText(getApplicationContext().getString(R.string.app_name))
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .build();
+            startForeground(22345, notification);
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
